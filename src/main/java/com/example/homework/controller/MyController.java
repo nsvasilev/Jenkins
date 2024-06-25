9cffc56c3e8913130784f199daa86c5b9b4af90d
Nikita Vasilyev <Nik.vasilev.20@inbox.ru>
2024-06-26 01:58:53 +0300

    aaaaaaaaaaaaaaaaa

--- src/main/java/com/example/homework/controller/MyController.java
+++ src/main/java/com/example/homework/controller/MyController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Controller {
+public class MyController {
 
     @GetMapping("/")
     public String home(Model model) {
